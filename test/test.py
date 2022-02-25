d45543f0c3698c4fda64ec92d9e6aebc7792ceec
panqinzhang <panqin.zhang@thoughtworks.com>
2022-02-25 13:40:11 +0800

    测试不同导包

--- test/test.py
+++ test/test.py
@@ -1,10 +1,10 @@
 import sys
+from os.path import abspath, dirname, join
 
+sys.path.insert(0, abspath(join(abspath(dirname(__file__)), '../')))
 # 当前目录和系统目录导包
-from ..package.demo.fileutil import test
+from package.demo.fileutil import test
 
 if __name__ == '__main__':
     print(sys.path)
     test()
-
-
